6860586a335a2746eeb6620daa78b45a0c33b07c
Filip Broniek <broniekf@gmail.com>
2021-12-07 12:55:58 +0100

    refresh

---
+++ Post.java
@@ -0,0 +1,14 @@
+package com.example.weatheraplication;
+
+public class Weather {
+    private String name;
+    private Main main;
+
+    public String getName() {
+        return name;
+    }
+
+    public Main getMain() {
+        return main;
+    }
+}
--- WeatherActivity.java
+++ WeatherActivity.java
@@ -1,9 +1,12 @@
 package com.example.weatheraplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -19,16 +22,39 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class WeatherActivity extends AppCompatActivity {
 
+    private SwipeRefreshLayout swipeRefreshLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-        TextView cityViev = findViewById(R.id.cityView);
         Intent intent = getIntent();
-        String cityNameToQuerry = intent.getStringExtra("KEY_CITY_NAME");
-        cityNameToQuerry = cityNameToQuerry + ",pl";
+        String cityNameToQuerry = intent.getStringExtra("KEY_CITY_NAME") + ",pl";
 
+        reRun(cityNameToQuerry);
+
+        swipeRefreshLayout = findViewById(R.id.swipe_layout);
+        initializeRefreshListener(cityNameToQuerry);
+    }
+
+    private void reRun(String cityNameToQuerry) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        getData(cityNameToQuerry);
+                        Thread.sleep(5000);
+                    }
+                } catch(InterruptedException e){
+                    Log.e("tagErr", e.getMessage());
+                }
+            }
+        }).start();
+    }
+
+    private void getData(String cityNameToQuerry) {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://api.openweathermap.org/data/2.5/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -36,42 +62,42 @@ public class WeatherActivity extends AppCompatActivity {
 
         JsonPlaceholderAPI jsonPlaceholderAPI = retrofit.create(JsonPlaceholderAPI.class);
 
-        Call<Weather> call = jsonPlaceholderAPI.getWeather(cityNameToQuerry, "749561a315b14523a8f5f1ef95e45864", "metric");
+        Call<Post> call = jsonPlaceholderAPI.getWeather(cityNameToQuerry, "749561a315b14523a8f5f1ef95e45864", "metric");
 
-        call.enqueue(new Callback<Weather>() {
+        call.enqueue(new Callback<Post>() {
             @Override
-            public void onResponse(Call<Weather> call, Response<Weather> response) {
+            public void onResponse(Call<Post> call, Response<Post> response) {
                 if (!response.isSuccessful()) {
                     Intent intent = new Intent(WeatherActivity.this, MainActivity.class);
                     startActivity(intent);
                     return;
                 }
-                Weather weather = response.body();
-                String cityName = weather.getName();
+                Post post = response.body();
+                String cityName = post.getName();
                 TextView cityViev = findViewById(R.id.cityView);
                 cityViev.setText(cityName);
 
-                Double tempValue = weather.getMain().getTemp();
+                Double tempValue = post.getMain().getTemp();
                 String tempValueReading = tempValue.toString();
                 TextView tempView = findViewById(R.id.tempView);
                 tempView.setText(tempValueReading+" °C");
 
-                Integer pressureValue = weather.getMain().getPressure();
+                Integer pressureValue = post.getMain().getPressure();
                 String pressureValueReading = pressureValue.toString();
                 TextView pressureView = findViewById(R.id.pressureView);
                 pressureView.setText(pressureValueReading+" hPa");
 
-                Integer humidityValue = weather.getMain().getHumidity();
+                Integer humidityValue = post.getMain().getHumidity();
                 String humidityValueReading = humidityValue.toString();
                 TextView humidityView = findViewById(R.id.humidityView);
                 humidityView.setText(humidityValueReading+" %");
 
-                Double tempMinValue = weather.getMain().getTemp_min();
+                Double tempMinValue = post.getMain().getTemp_min();
                 String tempMinValueReading = tempMinValue.toString();
                 TextView tempMinView = findViewById(R.id.tempMinView);
                 tempMinView.setText(tempMinValueReading+" °C");
 
-                Double tempMaxValue = weather.getMain().getTemp_max();
+                Double tempMaxValue = post.getMain().getTemp_max();
                 String tempMaxValueReading = tempMaxValue.toString();
                 TextView tempMaxView = findViewById(R.id.tempMaxView);
                 tempMaxView.setText(tempMaxValueReading+" °C");
@@ -83,14 +109,27 @@ public class WeatherActivity extends AppCompatActivity {
                 timeView.setText(currentTimeText);
 
                 saveCityName(cityName);
+
+
             }
 
             @Override
-            public void onFailure(Call<Weather> call, Throwable t) {
+            public void onFailure(Call<Post> call, Throwable t) {
 
             }
         });
     }
+
+    private void initializeRefreshListener(String cityName) {
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getData(cityName);
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+    }
+
     public void saveCityName(String cityName){
         SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
